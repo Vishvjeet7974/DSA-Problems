12df80bc233081e273406d1c20582e1b83a758fd
vishvjeet sing panwar <visvhjeetrajput0000@gmail.com>
2025-02-26 11:38:40 +0530

    Reverse a linkendList using Recursion

---
+++ LinkedList/Reverse a LinkedList using Recursion.java
@@ -0,0 +1,13 @@
+Node reverseList(Node head) {
+
+    if (head == null || head.next == null)
+        return head;
+
+    Node newNode = reverseList(head.next);
+
+    head.next.next = head;
+
+    head.next = null;
+
+    return newNode;
+}
